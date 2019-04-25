7cd646a6fab616f47b0e924ec19537beea065e8c
sylvainonweb <sylvain.onweb@gmail.com>
2019-04-25 14:03:54 +0200

    Utilisation de JQuery pour les tables => datatable

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,52 +10,47 @@
 
     initTable: function (tableId) {
 
-        // FIXME : Attention, aujourd'hui, si j'appelle cette fonction dans OnInitAsync, ça plante
-        // donc... Voir https://github.com/aspnet/AspNetCore/issues/8274
-        //$(document).ready(function () {
+        var table = $('#' + tableId).DataTable({
+                //pagingType: "simple",
 
-            var table = $('#' + tableId).DataTable({
-                 pagingType: "simple",
+            // Traduction
+            oLanguage: {
+                sSearch: "Rechercher",
+                sInfo: "de _START_ à _END_ sur _TOTAL_",
+                oPaginate: {
+                    sPrevious: "Préc.",
+                    sNext: "Suiv.",
+                    sLengthMenu: "_MENU_"
+                }
+            },
 
-                // Traduction
-                oLanguage: {
-                    sSearch: "Rechercher",
-                    sInfo: "de _START_ à _END_ sur _TOTAL_",
-                    oPaginate: {
-                        sPrevious: "Préc.",
-                        sNext: "Suiv.",
-                        sLengthMenu: "_MENU_"
+            // Définition des éléments de la grille à afficher
+            dom: 'frtipB',
+
+            // Customisation du bouton d'export Excel (icone, couleur, ...)
+            buttons: [{
+                extend: 'excel',
+                text: "<span class='fa fa-download' aria-hidden='true'></span> Exporter",
+                className: 'btn btn-success excelButton',
+                exportOptions: {
+                    modifier: {
+                        page: 'all'
                     }
-                },
+                }
+            }]
+        });
 
-                // Définition des éléments de la grille à afficher
-                dom: 'frtipB',
-
-                // Customisation du bouton d'export Excel (icone, couleur, ...)
-                buttons: [{
-                    extend: 'excel',
-                    text: "<span class='fa fa-download' aria-hidden='true'></span> Exporter",
-                    className: 'btn btn-success excelButton',
-                    exportOptions: {
-                        modifier: {
-                            page: 'all'
-                        }
-                    }
-                }]
-            });
-
-            // Ajout de filtres de type ComboBox sur l'ensemble des colonnes
-            $(".headerFilter").each(function (i) {
-                var select = $('<select class="custom-select"><option value=""></option></select>')
-                    .appendTo($(this).empty())
-                    .on('change', function () {
-                        var term = $(this).val();
-                        table.column(i).search(term, false, false).draw();
-                    });
-                table.column(i).data().unique().sort().each(function (d, j) {
-                    select.append('<option value="' + d + '">' + d + '</option>')
+        // Ajout de filtres de type ComboBox sur l'ensemble des colonnes
+        $(".headerFilter").each(function (i) {
+            var select = $('<select class="custom-select"><option value=""></option></select>')
+                .appendTo($(this).empty())
+                .on('change', function () {
+                    var term = $(this).val();
+                    table.column(i).search(term, false, false).draw();
                 });
+            table.column(i).data().unique().sort().each(function (d, j) {
+                select.append('<option value="' + d + '">' + d + '</option>')
             });
-        //});
+        });
     }
 };
