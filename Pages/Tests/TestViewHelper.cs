ce1a59a59121d8deb16d2b71c56e08ca166c0bb2
sylvainonweb <sylvain.onweb@gmail.com>
2019-04-26 12:04:47 +0200

    Ajout commentaires sur test des librairies

---
+++ Pages/Tests/TestViewHelper.cs
@@ -0,0 +1,13 @@
+using System;
+using Telerik.Blazor.Components.TabStrip;
+
+namespace BlazorTests.Pages.Tests
+{
+    public class TestViewHelper
+    {
+        public TestViewHelper()
+        {
+            TelerikTab tab = new TelerikTab();
+        }
+    }
+}
