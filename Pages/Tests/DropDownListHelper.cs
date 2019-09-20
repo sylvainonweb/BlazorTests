8c913d9ff196e3340d3c566e8a1f37aac18702bd
sylvainonweb <sylvain.onweb@gmail.com>
2019-09-20 10:40:59 +0200

    Telerik : test dropdownlist avec ligne vide

---
+++ Pages/Tests/DropDownListHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTests.Pages.Tests
+{
+    public class DropDownListItem
+    {
+        public int? Id { get; set; }
+        public string Text { get; set; }
+    }
+
+    /// <summary>
+    /// Nécessaire pour gérer les nullables
+    /// </summary>
+    public class DropDownListItemHelper
+    {
+        public static IList<DropDownListItem> Convert<T>(IList<T> data, Action<T, DropDownListItem> transformation, bool addEmptyLine = true)
+        {
+            IList<DropDownListItem> items = new List<DropDownListItem>();
+            if (addEmptyLine)
+            {
+                items.Add(new DropDownListItem { Id = default(int?), Text = string.Empty });
+            }
+
+            foreach (T datum in data)
+            {
+                DropDownListItem item = new DropDownListItem();
+                transformation.Invoke(datum, item);
+                items.Add(item);
+            }
+
+            return items;
+        }
+    }
+}
